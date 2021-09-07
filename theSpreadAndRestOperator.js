590a6b75334457945d34696c09969baf401fbcee
Mukunda Priya <mukundapriyab@gmail.com>
2021-09-07 02:30:49 +0100

    spread operator

--- theSpreadAndRestOperator.js
+++ theSpreadAndRestOperator.js
@@ -4,3 +4,13 @@ const newNumbers = [number, 6];
 
 console.log(numbers);
 console.log(newNumbers);
+
+const person = {
+  name: "Priya",
+};
+const newPerson = {
+  ...person,
+  age: 28,
+};
+
+console.log(newPerson);
